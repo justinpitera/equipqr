a817d517592340e9534b049e71c697c7b29422a3
Justin N. Pitera <justinpitera@gmail.com>
2024-11-24 23:57:58 +0000

    Added enum for crew position

---
+++ api/src/enums.py
@@ -0,0 +1,17 @@
+"""
+src/enums.py - This file contains various enums used throughout the api.
+
+Date: November 24, 2024
+
+Authors:
+    Justin N. Pitera (justinpitera@gmail.com)
+"""
+
+# Standard
+from enum import Enum
+
+
+class CrewMemberPositionEnum(str, Enum):
+    """Enum to describe crew member positions."""
+    GROUND_CREW   = "Ground Crew"
+    MECHANIC_CREW = "Mechanic Crew"
--- api/src/models/CrewMember.py
+++ api/src/models/CrewMember.py
@@ -1,5 +1,5 @@
 """
-src/models/CrewMember.py - Tortoise-ORM model for Crew Memembers.
+src/models/CrewMember.py - Tortoise-ORM model for Crew Members.
 
 Date: November 24, 2024
 
@@ -15,15 +15,19 @@ from tortoise.fields import (
     Field,
     UUIDField,
     CharField,
+    CharEnumField,
 )
 from tortoise.models import Model
+
+# Local
+from src.enums import CrewMemberPositionEnum
 
 
 class CrewMember(Model):
     """Database model to represent various types of crew memberss (ground crew, mechanics, etc) and degree of authority."""
     id                               : Field[UUID] = UUIDField(primary_key=True, unique=True, null=False)
     email                            : Field[str] = CharField(unique=True, max_length=255, null=False, db_index=True)
-    first_name                       : Field[str] = CharField(max_length=50, null=False)
-    last_name                        : Field[str] = CharField(max_length=50, null=False)
+    last_name                        : Field[str] = CharField(max_length=50, null=False) # TODO: Maybe drop, is email sufficient?
+    first_name                       : Field[str] = CharField(max_length=50, null=False) # TODO: Maybe drop, is email sufficient?
     language_preference              : Field[str] = CharField(max_length=50, null=False)
-    position                         : Field[str] = CharField(max_length=255, null=False) # TODO: Change to enum-based type.
+    position                         : CrewMemberPositionEnum = CharEnumField(CrewMemberPositionEnum, null=False)
