4149775dc343f514e5cc3f3eb05aee058b63fa9c
Burgil <41600149+burgil@users.noreply.github.com>
2024-12-07 07:59:35 +0200

    PWA install prompt

--- svelte/src/lib/helpers/register-sw.ts
+++ svelte/src/lib/helpers/register-sw.ts
@@ -29,4 +29,17 @@ const registerSw = (
 
 export async function registerServiceWorker() {
 	await registerSw("sw.js", /* 12 hours */ 1000 * 60 * 60 * 12);
+	window.addEventListener('beforeinstallprompt', (e) => {
+		// Prevent the default prompt from appearing automatically
+		e.preventDefault();
+		setTimeout(() => {
+			// @ts-ignore
+			e.prompt();
+		}, 2000);
+	});
+	// Optionally track PWA install events
+	window.addEventListener('appinstalled', () => {
+		alert('PWA was installed');
+	});
+
 }
