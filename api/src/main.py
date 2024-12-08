d8c50ac208d4c881d5a1b7d920b59d90466a9478
Justin N. Pitera <justinpitera@gmail.com>
2024-12-08 20:01:54 +0000

    ssl cert and key auto detection

--- api/src/main.py
+++ api/src/main.py
@@ -26,20 +26,47 @@ from src.asgi import init_asgi
 _: Starlette = init_asgi()
 init_colorama(autoreset=True)
 
+def _find_certificate_key_pair(directory: Path) -> tuple[Path, Path]:
+    """
+    Searches for a certificate and key pair within a given directory.
+    """
+    if not directory.is_dir():
+        raise ValueError(f"{directory} is not a valid directory.")
+
+    cert_ext: set[str] = {".crt", ".pem"}
+    key_ext: set[str] = {".key", ".pem"}
+    cert_file: Path | None = None
+    key_file: Path | None = None
+
+    for file in directory.iterdir():
+        if file.is_file():
+            if file.suffix in cert_ext and not cert_file:
+                cert_file = file
+            elif file.suffix in key_ext and not key_file:
+                key_file = file
+
+            if cert_file and key_file:
+                return cert_file, key_file
+
+    raise ValueError("Valid certificate and key files not found in the directory.")
+
 
 async def _main() -> None:
+    ssl_cert, ssl_key = _find_certificate_key_pair(directory=Path(API_CONFIG["api"]["ssl_directory"]))
+
     _GRANIAN_SERVER: Granian = Granian(
         target="src.main:_",
-        address=API_CONFIG["api"]["address"],
+        address=str(API_CONFIG["api"]["address"]),
         interface=Interfaces.ASGI,
-        log_enabled=True,            
+        log_enabled=True,
         log_level=LogLevels.warn,
-        port=API_CONFIG["api"]["port"],
-        ssl_cert=Path(API_CONFIG["api"]["ssl_certfile"]),
-        ssl_key=Path(API_CONFIG["api"]["ssl_keyfile"]),
+        port=int(API_CONFIG["api"]["port"]),
+        ssl_cert=ssl_cert,
+        ssl_key=ssl_key,
         process_name="aviator_fjelmelings"
     )
     _GRANIAN_SERVER.serve()
+
 
 if __name__ == "__main__":
     try:
