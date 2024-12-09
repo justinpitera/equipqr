45db0ae09f7c5344924c891d702114440788c15f
Burgil <41600149+burgil@users.noreply.github.com>
2024-12-09 20:25:49 +0200

    fixed login

--- svelte/src/lib/config.ts
+++ svelte/src/lib/config.ts
@@ -1,4 +1,4 @@
-export const DEBUG_MODE = true; // Force popup to auto scan an example QR without a camera and prevent showing errors due to missing camera and auto navigate back to homepage from the cancel report
+export const DEBUG_MODE = false; // Force popup to auto scan an example QR without a camera and prevent showing errors due to missing camera and auto navigate back to homepage from the cancel report
 
 export const BACKEND_URL = 'https://preview.pitera.co:7878'; // empty in production
 
