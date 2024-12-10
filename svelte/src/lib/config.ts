55d28baafc5d827627c7856b7743e6f428da7712
Burgil <41600149+burgil@users.noreply.github.com>
2024-12-10 03:12:57 +0200

    Update config.ts

--- svelte/src/lib/config.ts
+++ svelte/src/lib/config.ts
@@ -1,4 +1,4 @@
-export const DEBUG_MODE = false; //typeof window !== 'undefined' ? location.hostname === 'localhost' : false; // Force popup to auto scan an example QR without a camera and prevent showing errors due to missing camera and auto navigate back to homepage from the cancel report
+export const DEBUG_MODE = typeof window !== 'undefined' ? location.hostname === 'localhost' : false; // Force popup to auto scan an example QR without a camera and prevent showing errors due to missing camera and auto navigate back to homepage from the cancel report
 
 export const defaultLang = 'en';
 
