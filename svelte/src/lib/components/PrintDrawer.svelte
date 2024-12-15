dd9665e4617aff718b47fabd76ff6ef875443aa0
Burgil <41600149+burgil@users.noreply.github.com>
2024-12-15 17:17:29 +0200

    Update PrintDrawer.svelte

--- svelte/src/lib/components/PrintDrawer.svelte
+++ svelte/src/lib/components/PrintDrawer.svelte
@@ -266,7 +266,7 @@
 
         <div class="mb-4 mt-2">
             <div
-                class="h-[calc(100svh-(104px+36px+16px+24px+24px+8px))] overflow-y-auto overflow-x-hidden pb-3"
+                class="h-[calc(100svh-185px)] overflow-y-auto overflow-x-hidden pb-3"
             >
                 <div
                     class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 justify-items-center gap-6"
