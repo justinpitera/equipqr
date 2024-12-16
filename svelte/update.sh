2eef32764405d1f48ba78e7e0835cb461ac784c3
Burgil <41600149+burgil@users.noreply.github.com>
2024-12-16 12:41:25 +0200

    Added a workspace

--- svelte/update.sh
+++ svelte/update.sh
@@ -1,3 +1,4 @@
+# Ew brother whats this haha just run `bun update --latest` or something lol
 for pkg in $(jq -r '.dependencies + .devDependencies | keys[]' package.json); do
     bun add "$pkg@latest"
 done
