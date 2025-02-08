8bf3be0646abd7aae196e6e058d1a66e919cbdc0
Burgil‬‏ <41600149+burgil@users.noreply.github.com>
2025-02-09 00:04:08 +0200

    Better FPS

--- svelte/src/lib/helpers/camera.ts
+++ svelte/src/lib/helpers/camera.ts
@@ -80,7 +80,7 @@ const getCameraWithTorchInfo = async (customDevice?: string): Promise<ITorchInfo
 					deviceId: { exact: device.deviceId },
 					// width: { ideal: 4096 },
 					// height: { ideal: 2160 },
-					// frameRate: { ideal: 60 }
+					frameRate: { ideal: 60 }
 				},
 				// audio: true
 			});
