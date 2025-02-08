8a1d6f9295de2c1fb1adbd7b54cf9ed1364eccd4
Burgil‬‏ <41600149+burgil@users.noreply.github.com>
2025-02-09 00:20:29 +0200

    Update camera.ts

--- svelte/src/lib/helpers/camera.ts
+++ svelte/src/lib/helpers/camera.ts
@@ -78,10 +78,8 @@ const getCameraWithTorchInfo = async (customDevice?: string): Promise<ITorchInfo
 				video: {
 					facingMode: "environment",
 					deviceId: { exact: device.deviceId },
-					// width: { ideal: 1280 },  // 720p width
-					// height: { ideal: 720 },  // 720p height
-					width: { ideal: 99999 },
-                	height: { ideal: 99999 },
+					width: { ideal: 1280 },  // 720p width
+					height: { ideal: 720 },  // 720p height
 					// aspectRatio: { ideal: 1 }, // 16:9 aspect ratio
 					// frameRate: { ideal: 30, max: 30 }
 				}
